050ebdffe7680e8ba40a699e86353797d8f67bf2
nicolas-trouin <nicolas.trouin@insa-lyon.fr>
2020-05-19 11:07:29 +0200

    Description client dynamique avec historique

--- java/src/main/java/fr/insalyon/dasi/ihm/web/action/GetConsultationEnAttenteAction.java
+++ java/src/main/java/fr/insalyon/dasi/ihm/web/action/GetConsultationEnAttenteAction.java
@@ -1,5 +1,6 @@
 package fr.insalyon.dasi.ihm.web.action;
 
+import fr.insalyon.dasi.metier.modele.Client;
 import fr.insalyon.dasi.metier.modele.Consultation;
 import fr.insalyon.dasi.metier.modele.Employe;
 import fr.insalyon.dasi.metier.service.Service;
@@ -15,6 +16,9 @@ public class GetConsultationEnAttenteAction extends Action{
         Long idEmploye = (Long) session.getAttribute("idEmploye");
         Consultation consultation = null;
         Employe employe = null;
+        Client client = null;
+        List<Consultation> historiqueClient = null;
+        
         if(idEmploye != null){
             Service service = new Service();
             employe = service.rechercherEmployeParId(idEmploye);
@@ -22,11 +26,16 @@ public class GetConsultationEnAttenteAction extends Action{
             if(employe.getEstOccupe()){
                 List<Consultation> consultations = employe.getConsultations();
                 consultation = consultations.get(consultations.size()-1);
-                System.out.println(consultation);
+                client = consultation.getClient();
+                if(client.getConsultations() != null && client.getConsultations().size() >=1){
+                    historiqueClient = client.getConsultations();
+                }
             }
         }
         request.setAttribute("consultation", consultation);
         request.setAttribute("employe", employe);
+        request.setAttribute("client", client);
+        request.setAttribute("historiqueClient", historiqueClient);
     }
     
 }
